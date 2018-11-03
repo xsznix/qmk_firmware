a2cfc1feaa470c7abeae686eb0972465f13a2fd2
Xuming Zeng <xsznix@gmail.com>
2018-11-03 11:23:42 -0700

    Move backslash on qwerty to not interfere with left click on base layer

--- keyboards/ergodox_ez/keymaps/rsthd/keymap.c
+++ keyboards/ergodox_ez/keymaps/rsthd/keymap.c
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |   \  |      |      |      |                                       |      |      |      |      |      |
+ *   |      |      |      |      |      |                                       |      |      |   \  |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_TRNS,
        KC_TRNS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
        KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_TRNS,
-       KC_TRNS, KC_BSLS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                         KC_TRNS, KC_TRNS,
                                                  KC_TRNS,
                                 KC_SPC, KC_TRNS, KC_TRNS,
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_Y, KC_U,    KC_I,   KC_O,    KC_P,     KC_MINUS,
                 KC_H, KC_J,    KC_K,   KC_L,    KC_SCLN,  KC_QUOT,
        KC_TRNS, KC_N, KC_M,    KC_COMM,KC_DOT,  KC_SLSH,  KC_TRNS,
-                      KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_TRNS,
+                      KC_TRNS, KC_TRNS, KC_BSLS,KC_TRNS,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
